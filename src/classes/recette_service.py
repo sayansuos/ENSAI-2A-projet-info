1ef0924ff367461b6abb26902ad769d6f39779b4
Rayane <rayane.djidjelli@eleve.ensai.fr>
2024-10-11 14:38:30 +0200

    Code squelette des classes

--- src/classes/recette_service.py
+++ src/classes/recette_service.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 from src.classes.recette import Recette
+from src.dao.db_connection import DBConnection
 
 
 class RecetteService(Recette):
@@ -7,13 +8,86 @@ class RecetteService(Recette):
     Définis les méthodes de la classe Recette
     """
 
-    def trouver_recette_par_nom(nom: str) -> Recette:
-        pass
+    def trouver_recette_par_nom(self, nom: str) -> Optional[Recette]:
+        """
+        Permet de trouver une recette en indiquant le nom de celle-ci
 
-    def trouver_recette_par_ingredient(ingredient: str) -> list[Recette]:
-        pass
+        Args:
+            nom (str): Nom de la recette recherchée
 
-    def lister_toutes_recettes() -> list[Recette]:
+        Returns:
+            Optional[Recette]: Renvoie la recette si le nom correspond à une
+                               recette existante. Renvoie None sinon
+        """
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "SELECT *      "
+                    "FROM Recette  "
+                    "WHERE nom_recette = %(nom)s",
+                    {"nom": nom}
+                )
+
+                res = cursor.fetchone()
+
+        if res:
+            id = res["id"]
+            nom = res["nom"]
+            liste_ingredient = res["liste_ingredient"]
+
+            res = Recette(id_recette=id,
+                          nom_recette=nom,
+                          liste_ingredient=liste_ingredient)
+
+        return res
+
+    def trouver_recette_par_ingredient(ingredient: str) -> List[Recette]:
+        """
+        Permet de trouver une liste de recette contenant l'ingrédient entré
+        en paramètre
+
+        Args:
+            ingredient (str): Nom de l'ingrédient recherché
+
+        Returns:
+            list[Recette]: Renvoie la liste des recettes contenant cet ingrédient
+        """
+
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                # La requête SQL est à trouver
+                cursor.execute(
+                    "SELECT *      "
+                    "FROM Recette  "
+                    "WHERE "
+                )
+
+                res = cursor.fetchall()
+
+        Liste_recettes = []
+
+        if res:
+            for row in res:
+                id = row["id"]
+                nom = row["nom"]
+                liste_ingredient = row["liste_ingredient"]
+
+                recette = Recette(id_recette=id,
+                                  nom_recette=nom,
+                                  liste_ingredient=liste_ingredient)
+
+                Liste_recettes.append(recette)
+
+        return Liste_recettes
+
+    def lister_toutes_recettes() -> List[Recette]:
+        """
+        Renvoie une liste de toutes les recettes existantes.
+
+        Returns:
+            list[Recette]: Liste de toutes les recettes existantes
+        """
+
         with DBConnection().connection as connection:
             with connection.cursor() as cursor:
                 cursor.execute(
@@ -23,6 +97,17 @@ class RecetteService(Recette):
 
                 res = cursor.fetchall()
 
+        Liste_recettes = []
         if res:
             for row in res:
-                pass
+                id = row["id"]
+                nom = row["nom"]
+                liste_ingredient = row["liste_ingredient"]
+
+                recette = Recette(id_recette=id,
+                                  nom_recette=nom,
+                                  liste_ingredient=liste_ingredient)
+
+                Liste_recettes.append(recette)
+
+        return Liste_recettes
