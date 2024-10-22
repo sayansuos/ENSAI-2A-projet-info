db10792118ffa5bc76bb0d4f9c366b4c3f00764f
Nora Aankoud <nora.aankoud@eleve.ensai.fr>
2024-10-22 15:21:00 +0200

    utilisateur_dao + test utilisateur_dao

--- data/pop_db_test.sql
+++ data/pop_db_test.sql
@@ -1,8 +1,8 @@
-INSERT INTO joueur(id_joueur, pseudo, mdp, age, mail, fan_pokemon) VALUES
-(999, 'admin',      '0000',     0,       'admin@projet.fr',      null),
-(998, 'a',             'a',     20,      'a@ensai.fr',           true),
-(997, 'maurice',    '1234',     20,      'maurice@ensai.fr',     true),
-(996, 'batricia',   '9876',     25,      'bat@projet.fr',        false),
-(995, 'miguel',     'abcd',     23,      'miguel@projet.fr',     true),
-(994, 'gilbert',    'toto',     21,      'gilbert@projet.fr',    false),
-(993, 'junior',     'aaaa',     15,      'junior@projet.fr',     true);
+INSERT INTO utilisateur(id_utilisateur, pseudo, mdp, mail, role_utilisateur) VALUES
+(1, 'admin', '0000', 'admin@projet.fr', 'admin'),
+(2, 'a', 'a', 'a@ensai.fr', 'user'),
+(3, 'maurice', '1234', 'maurice@ensai.fr', 'user'),
+(4, 'batricia', '9876', 'bat@projet.fr', 'user'),
+(5, 'miguel', 'abcd', 'miguel@projet.fr', 'user'),
+(6, 'gilbert', 'toto', 'gilbert@projet.fr', 'user'),
+(7, 'junior', 'aaaa', 'junior@projet.fr', 'user');
