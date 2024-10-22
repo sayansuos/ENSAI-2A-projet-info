ef3f5add0de989a655f90009feb86843186dea62
NolramD <marlondujj@gmail.com>
2024-10-22 20:48:25 +0200

    vues invité recettes filtre et sans

--- src/view/recette/menu_recette_inv_af.py
+++ src/view/recette/menu_recette_inv_af.py
@@ -1,10 +1,11 @@
 from InquirerPy import inquirer
 
 from view.vue_abstraite import VueAbstraite
+from src.service.ingredient_service import IngredientService
 from src.service.recette_service import RecetteService
 
 
-class MenuRecetteSf(VueAbstraite):
+class MenuRecetteAf(VueAbstraite):
     """Vue qui affiche :
     - toutes les recettes dispo selon l'ingredient choisi
     - les options
@@ -12,16 +13,23 @@ class MenuRecetteSf(VueAbstraite):
 
     def choisir_menu(self):
         recette_service = RecetteService()
+        ingredient_service = IngredientService
 
-        liste_recettes = recette_service.lister_toutes_recettes()
-        liste_recettes.append("Retour")
-
+        liste_ingredients = ingredient_service.lister_tous()
         choix = inquirer.select(
-            message="Choisissez une recette : ",
-            choices=liste_recettes,
+            message="Choisissez un ingrédient : ",
+            choices=liste_ingredients,
         ).execute()
 
-        if choix == "Retour":
+        liste_recette_filtree = recette_service.trouver_recette_par_ingredient(choix)
+        liste_recette_filtree.append("Retour")
+
+        choix_deux = inquirer.select(
+            message="Choisissez une recette : ",
+            choices=liste_recette_filtree,
+        ).execute()
+
+        if choix_deux == "Retour":
             from src.view.recettes.recettes_vue_inv import RecettesVue
 
             return RecettesVue()
@@ -37,10 +45,10 @@ class MenuRecetteSf(VueAbstraite):
             match choix_bis:
                 case "Lire la recette":
 
-                    return recette_service.voir_description(choix)
+                    return recette_service.voir_description(choix_deux)
                 
                 case "Voir les notes et les avis":
 
-                    return recette_service.voir_note(choix), recette_service.voir_avis(choix)
+                    return recette_service.voir_note(choix_deux), recette_service.voir_avis(choix_deux)
 
-        return MenuRecetteSf()
+        return MenuRecetteAf()
