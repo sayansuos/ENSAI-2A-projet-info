cc96cade0f4c7f621765750447646cefd9955345
Rayane <rayane.djidjelli@eleve.ensai.fr>
2024-10-22 11:40:33 +0200

    Ajout des méthodes de création/suppression de recette + méthode pour lister les recettes

--- src/classes/recette_service.py
+++ src/classes/recette_service.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 from src.classes.recette import Recette
-from src.dao.db_connection import DBConnection
+from src.dao.recette_dao import RecetteDao
 
 
 class RecetteService(Recette):
@@ -21,24 +21,9 @@ class RecetteService(Recette):
         """
         if not isinstance(nom, str):
             raise TypeError("nom doit être une instance de str")
-        with DBConnection().connection as connection:
-            with connection.cursor() as cursor:
-                cursor.execute(
-                    "SELECT *      " "FROM Recette  " "WHERE nom_recette = %(nom)s", {"nom": nom}
-                )
+        pass
 
-                res = cursor.fetchone()
-
-        if res:
-            id = res["id"]
-            nom = res["nom"]
-            liste_ingredient = res["liste_ingredient"]
-
-            res = Recette(id_recette=id, nom_recette=nom, liste_ingredient=liste_ingredient)
-
-        return res
-
-    def trouver_recette_par_ingredient(ingredient: str) -> List[Recette]:
+    def trouver_recette_par_ingredient(self, ingredient: str) -> List[Recette]:
         """
         Permet de trouver une liste de recette contenant l'ingrédient entré
         en paramètre
@@ -49,30 +34,12 @@ class RecetteService(Recette):
         Returns:
             list[Recette]: Renvoie la liste des recettes contenant cet ingrédient
         """
+
         if not isinstance(ingredient, str):
             raise TypeError("ingredient doit être une instance de str")
-        with DBConnection().connection as connection:
-            with connection.cursor() as cursor:
-                # La requête SQL est à trouver
-                cursor.execute("SELECT *      " "FROM Recette  " "WHERE ")
+        pass
 
-                res = cursor.fetchall()
-
-        Liste_recettes = []
-
-        if res:
-            for row in res:
-                id = row["id"]
-                nom = row["nom"]
-                liste_ingredient = row["liste_ingredient"]
-
-                recette = Recette(id_recette=id, nom_recette=nom, liste_ingredient=liste_ingredient)
-
-                Liste_recettes.append(recette)
-
-        return Liste_recettes
-
-    def lister_toutes_recettes() -> List[Recette]:
+    def lister_toutes_recettes(self) -> List[Recette]:
         """
         Renvoie une liste de toutes les recettes existantes.
 
@@ -80,21 +47,75 @@ class RecetteService(Recette):
             list[Recette]: Liste de toutes les recettes existantes
         """
 
-        with DBConnection().connection as connection:
-            with connection.cursor() as cursor:
-                cursor.execute("SELECT *      " "FROM Recette  ")
+        return RecetteDao.lister_tous()
 
-                res = cursor.fetchall()
+    def noter_recette(self, note: float):
+        """
+        Permet de noter une recette de 0 à 5.
 
-        Liste_recettes = []
-        if res:
-            for row in res:
-                id = row["id"]
-                nom = row["nom"]
-                liste_ingredient = row["liste_ingredient"]
+        Args:
+            note (float): Note donnée à la recette. Les notes vont de 0 à 5, avec un pas de 0.5
+        """
 
-                recette = Recette(id_recette=id, nom_recette=nom, liste_ingredient=liste_ingredient)
+        if not (isinstance(note, float) or isinstance(note, int)):
+            raise TypeError("La note doit être un nombre compris entre 0 et 5.")
+        if note < 0 or note > 5:
+            raise ValueError("La note doit être comprise entre 0 et 5.")
 
-                Liste_recettes.append(recette)
+        pass
 
-        return Liste_recettes
+    def commenter_recette(self, commentaire: str):
+        """
+        Permet de commenter une recette
+
+        Args:
+            commentaire (str): Avis de l'utilisateur sur la recette
+        """
+
+        if not isinstance(commentaire, str):
+            raise TypeError("Le commentaire doit être une chaîne de caractères")
+
+        pass
+
+    def creer_recette(self, recette: Recette) -> Optional[Recette]:
+        """
+        Permet d'ajouter une recette dans la base de données.
+
+        Args:
+            recette (Recette): Informations de la recette à ajouter
+
+        Returns:
+            Optional[Recette]:
+                Retourne la recette si elle a été correctement ajoutée à la base de données
+                None sinon
+        """
+
+        return recette if RecetteDao.creer(recette) is False else None
+
+    def supprimer_recette(self, recette: Recette) -> bool:
+        """
+        Permet de supprimer une recette de la base de données
+
+        Args:
+            recette (Recette): Recette à supprimer
+
+        Returns:
+            bool:
+                True si la recette a été correctement supprimée. False sinon
+        """
+
+        return RecetteDao.supprimer(recette)
+
+    def trouver_recette_par_id(self, id: int) -> Optional[Recette]:
+        """
+        Permet de trouver une recette par identifiant
+
+        Args:
+            id (int): Identifiant de la recette recherchée
+
+        Returns:
+            Optional[Recette]: Retourne la recette correspondant à l'identifiant.
+                                Retourne None sinon
+        """
+
+        return RecetteDao.trouver_par_id(id)
