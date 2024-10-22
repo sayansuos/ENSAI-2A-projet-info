b67076572c4cc1d67613a8fea5923930907f44b0
NolramD <marlondujj@gmail.com>
2024-10-22 21:02:56 +0200

    vues recettes user

--- src/view/recette/menu_recette_user_af.py
+++ src/view/recette/menu_recette_user_af.py
@@ -1,29 +1,37 @@
 from InquirerPy import inquirer
 
 from view.vue_abstraite import VueAbstraite
+from src.service.ingredient_service import IngredientService
 from src.service.recette_service import RecetteService
 from src.service.liste_favoris_service import ListeFavorisService
 lfs = ListeFavorisService()
 
 
-class MenuRecetteSf(VueAbstraite):
+class MenuRecetteAf(VueAbstraite):
     """Vue qui affiche :
-    - toutes les recettes dispo
+    - toutes les recettes dispo selon l'ingredient choisi
     - les options
     """
 
     def choisir_menu(self):
         recette_service = RecetteService()
+        ingredient_service = IngredientService
 
-        liste_recettes = recette_service.lister_toutes_recettes()
-        liste_recettes.append("Retour")
-
+        liste_ingredients = ingredient_service.lister_tous()
         choix = inquirer.select(
-            message="Choisissez une recette : ",
-            choices=liste_recettes,
+            message="Choisissez un ingrédient : ",
+            choices=liste_ingredients,
         ).execute()
 
-        if choix == "Retour":
+        liste_recette_filtree = recette_service.trouver_recette_par_ingredient(choix)
+        liste_recette_filtree.append("Retour")
+
+        choix_deux = inquirer.select(
+            message="Choisissez une recette : ",
+            choices=liste_recette_filtree,
+        ).execute()
+
+        if choix_deux == "Retour":
             from src.view.recettes.recettes_vue_user import RecettesVue
 
             return RecettesVue()
@@ -43,11 +51,11 @@ class MenuRecetteSf(VueAbstraite):
             match choix_bis:
                 case "Lire la recette":
 
-                    return recette_service.voir_description(choix)
+                    return recette_service.voir_description(choix_deux)
                 
                 case "Voir les notes et les avis":
 
-                    return recette_service.voir_note(choix), recette_service.voir_avis(choix)
+                    return recette_service.voir_note(choix_deux), recette_service.voir_avis(choix_deux)
                 
                 case "Noter et laisser un commentaire":
 
@@ -55,15 +63,14 @@ class MenuRecetteSf(VueAbstraite):
 
                 case "Ajouter dans les favoris":
 
-                    return lfs.ajouter_favoris(choix)
+                    return lfs.ajouter_favoris(choix_deux)
                 
                 case "Supprimer des favoris":
 
-                    return lfs.supprimer_favoris(choix)
+                    return lfs.supprimer_favoris(choix_deux)
                 
                 case "Ajouter les ingredients au panier":
-                    ingr = recette_service.ingredients_recette(choix)
+                    ingr = recette_service.ingredients_recette(choix_deux)
 
                     return lfs.ajouter_liste_course(ingr)
-
-        return MenuRecetteSf()
+        return MenuRecetteAf()
