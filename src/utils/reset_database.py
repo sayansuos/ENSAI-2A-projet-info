7f3870144c0658439701f6bcb3bcd25e00220c1e
SUOS Sayan <id2363@DOMENSAI.ECOLE>
2024-11-03 16:42:24 +0100

    liste favoris dao et service

--- src/utils/reset_database.py
+++ src/utils/reset_database.py
@@ -18,11 +18,11 @@ class ResetDatabase(metaclass=Singleton):
     def lancer(self, test_dao=False):
         """Lancement de la réinitialisation des données
         Si test_dao = True : réinitialisation des données de test"""
-        if test_dao:
-            mock.patch.dict(os.environ, {"POSTGRES_SCHEMA": "projet_test_dao"}).start()
-            pop_data_path = "data/pop_db_test.sql"
-        else:
-            pop_data_path = "data/pop_db.sql"
+        # if test_dao:
+        #    mock.patch.dict(os.environ, {"POSTGRES_SCHEMA": "projet_test_dao"}).start()
+        #    pop_data_path = "data/pop_db_test.sql"
+        # else:
+        #    pop_data_path = "data/pop_db.sql"
 
         dotenv.load_dotenv()
 
@@ -30,7 +30,7 @@ class ResetDatabase(metaclass=Singleton):
 
         create_schema = f"DROP SCHEMA IF EXISTS {schema} CASCADE; CREATE SCHEMA {schema};"
 
-        pop_db = open(pop_data_path, encoding="utf-8")
+        pop_db = open("data/pop_db.sql", encoding="utf-8")
         pop_db_as_string = pop_db.read()
         pop_db.close()
 
